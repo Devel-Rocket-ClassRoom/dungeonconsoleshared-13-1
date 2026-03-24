43f313f2c89a9c592d1c73caf27febf68cd50eb4
seodongyeon <jrnre1@naver.com>
2026-03-24 13:53:13 +0900

    오타, 불필요한 코드 제거
    using중복 제거와 필요없는 주석들 제거

--- test/Program.cs
+++ test/Program.cs
@@ -4,26 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using System;
 
-//DungeonGame 클래스디자인
-//기존에 만든 텍스트 던전게임 코드를 DungeonGame 클래스로 이동
-//기존 Main()에서 구현한 내용을 DungeonGame 클래스에
-// 함수 PlayGame()를 만들어서 이동
-//기존 모든 관련 함수를 DungeonGame 클래스 내부로 이동
-//Main()에서 DungeonGame를 new로 만들고 PlayGame()호출
-//기존 게임과 동일하게 작동할 것.   
-
-//***던전게임 개선하기***1111
-//1.부모 클래스 (플레이어, 몬스터 공통)
-// - 공통 데이터, 메소드 선언
-//2.플레이어 , 몬스터 클래스 정의
-// = 각각 전용 데이터 선언
-//3.플레이어 전용데이터, 메소드 (함수)
-// - 예) 이동시 함수 사용
-//4.몬스터 전용 데이터,메소드
-//5.맵 클래스 만들고 코드정리
-namespace tset
+namespace test
 {
     internal class Program
     {
@@ -171,15 +153,6 @@ namespace tset
         Map gameMap = new Map();
         Player player = new Player();
 
-        //int[] aaa = new int[5];
-        //int[] aaa = new int[5];
-        //int[] aaa = new int[5];    
-        //char[] ccc = new char[5];
-        //GameData[] ddd = new GameData[5];
-
-        //List<GameData> stages = new List<GameData>();
-        //GameData data = new GameData("Intro",1);
-
         public DungeonGame(string name, int count)
         {
 
@@ -246,7 +219,7 @@ namespace tset
                         Console.WriteLine("이동 못함");
 
                     }
-
+                   
                     else if (gameMap.map[player.row + dirR, player.col + dirC] == 'M')
                     {
                         Console.WriteLine("게임종료");
@@ -255,10 +228,6 @@ namespace tset
                     else
                     {
                         gameMap.SetPlayer(dirR, dirC, player);
-                        //gameMap.map[player.row, player.col] = ' ';
-                        //player.row += dirR;
-                        //player.col += dirC;
-                        //gameMap.map[player.row, player.col] = 'P';
                     }
 
                     gameMap.PrintMap();
